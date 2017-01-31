ea4dbc0080dc6100527006cf61ed31508e93f494
prinsmike <mike.prinsloo@gmail.com>
2017-01-31 13:15:34 +0200

    Fixed missing backslash.

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 docker run -d --name caddy \
 	-v $HOME/srv:/srv \
-	-v $HOME/Caddyfiles:/etc/Caddyfiles
+	-v $HOME/Caddyfiles:/etc/Caddyfiles \
 	-v $HOME/.caddy:/root/.caddy \
 	-p 80:80 -p 443:443 \
 	prinsmike/caddy
